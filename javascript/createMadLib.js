7acf7e69748b37ea5fb79da877115b744c7626bd
Risa Fletcher <risa.fletcher@gmail.com>
2019-01-19 23:38:49 -0800

    added comments

--- javascript/createMadLib.js
+++ javascript/createMadLib.js
@@ -1,19 +1,29 @@
+// this function will return our input values, aka,
+// whatever we type in the input box
 function getInputValue(inputId) {
 	return document.getElementById(inputId).value;
 }
-
+// this function will just return our HTML element
 function getElement(elementId) {
 	return document.getElementById(elementId);
 }
 
 function libIt() {
-	let storyDiv = getElement('story');
+	// here we are collecting the values for each input box
+	// think of these variables as placeholders
 	let name = getInputValue('noun');
 	let adjective = getInputValue('adjective');
 	let noun = getInputValue('name');
-
+	// this is where we create our story
+	// we wrap each text block with back ticks (`) so we can
+	// combine our text and variables much easier
+	// notice how each variable (aka placeholder) is wrapped in ${}
 	let sentence1 = `${name} is a very ${adjective} ${noun}! `;
 	let sentence2 = `${name} is a very ${adjective} ${noun}!`;
-
+	// here we are getting our 'story' container and
+	// assigning it to storyDiv
+	let storyDiv = getElement('story');
+	// inside our story div, aka its innerHTML, we want to
+	// insert our text blocks
 	storyDiv.innerHTML = sentence1 + sentence2;
 }
